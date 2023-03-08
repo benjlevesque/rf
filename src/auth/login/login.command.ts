e0b20a2848d5c12aceed0f6b92228bfff4252c7e
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2023-03-08 22:05:34 +0100

    feat(login): display user's email

--- src/auth/login/login.command.ts
+++ src/auth/login/login.command.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { Command } from 'nestjs-command';
 
 import { AuthService } from '../auth.service';
+import { WhoamiService } from '../whoami/whoami.service';
 
 @Injectable()
 export class LoginCommand {
-  constructor(private readonly service: AuthService) {}
+  constructor(
+    private readonly service: AuthService,
+    private readonly whoami: WhoamiService,
+  ) {}
   @Command({ command: 'auth:login', describe: 'Login' })
   async run(): Promise<void> {
     const { createSpinner } = await import('nanospinner');
@@ -13,6 +17,9 @@ export class LoginCommand {
 
     spinner.start({ text: 'Logging in...' });
     await this.service.login();
-    spinner.success({ text: 'Success!' });
+
+    const { profile } = await this.whoami.whoami();
+
+    spinner.success({ text: `Success! Welcome ${profile.email}` });
   }
 }
